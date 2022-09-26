9e783d5b0546aed0b1e9357339e898d93f08afef
Anshu <anshukumarsingh448@gmail.com>
2022-09-26 09:43:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a7mintuesworkout/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.a7mintuesworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.a7mintuesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+       // val flButton : FrameLayout = findViewById(R.id.flStart)
+           binding?.flStart?.setOnClickListener {
+           /* Toast.makeText(this@MainActivity,
+                          "Here we will start the Exercise",
+                           Toast.LENGTH_LONG).show()*/
+               val intent = Intent(this,ExerciseActivity::class.java)
+               startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+}
